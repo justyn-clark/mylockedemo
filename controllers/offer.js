502fd2e4c3381018963831060f78581499326e18
ijustynclark@gmail.com <justyn@justynclark.com>
2016-11-15 02:10:48 -0800

    make it better

---
+++ controllers/offer.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//var passportConf  = require('../config/passport');  // New dependency
+
+module.exports.controller = function(app) {
+    app.get('/offer', function(req, res) {  // When user requests offer page
+        res.render('offer/offer', {           // Render hello page
+        });
+    });
+};
---
+++ models/Offer.js
@@ -0,0 +1,22 @@
+/**
+ * Offer
+ */
+
+/**
+ * Module Dependencies
+ */
+
+var mongoose = require('mongoose');
+
+
+var offerSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    amount: Number,
+    maximumRides: Number
+});
+
+
+
+
+module.exports = mongoose.model('Offer', offerSchema);
